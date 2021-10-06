a83e494d385d1d2141d9bc39396b95abf0bc38dd
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-10-06 16:45:54 -0500

    working text area

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,16 @@
 //display current date on the page
 const currentDate = moment().format("dddd, MMM Do, YY");
 const currentDateEl = document.querySelector("#currentDay");
-
 currentDateEl.textContent = currentDate;
+
+var tasks = {};
+
+$(".task").on("click", function(){
+  var text = $(this).text().trim();
+console.log(text);
+  
+  var textInput = $("<textarea>").addClass("col-8 col-md-8 task").val(text);
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
 
