07d4826d888217a55187143394deb47538c522cb
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-10-05 21:41:58 -0500

    add the row for the first hour

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,6 @@
+//display current date on the page
 const currentDate = moment().format("dddd, MMM Do, YY");
 const currentDateEl = document.querySelector("#currentDay");
 
 currentDateEl.textContent = currentDate;
+
