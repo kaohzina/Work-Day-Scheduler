0fb86021a996132c050bb1068fb22fa39cd6c066
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-10-06 17:57:09 -0500

    trying to get the save button to work

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,3 @@
-const moment = require("moment");
-
 //display current date on the page
 const currentDate = moment().format("dddd, MMM Do, YY");
 const currentDateEl = document.querySelector("#currentDay");
@@ -9,17 +7,32 @@ var tasks = {};
 
 $(".task").on("click", function(){
   var text = $(this).text().trim();
-console.log(text);
+  console.log(text);
   
   var textInput = $("<textarea>").addClass("col-8 col-md-8 task").val(text);
   $(this).replaceWith(textInput);
   textInput.trigger("focus");
+  
 });
+
+$(".saveBtn").on("click", function(){
+  $(this)
+  .find("task")
+  let savedTask 
+  saveTasks();
+  console.log("saved");
+})
+
+var saveTasks = function() {
+
+  localStorage.setItem("task", JSON.stringify(tasks).
+  textInput);
+};
 
 //track the times that have passed
 const past = function(){
 if(hour < currentTime) {
-
+var old = $("").addClass(".past").val(text);
   }
 }
 
@@ -33,7 +46,7 @@ if(hour === currentTime){
 //track the time in the future
 const future = function(){
   if(hour > currentTime) {
-    
+
   }
 }
 
